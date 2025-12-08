d01babc2d457a1e4a6055bb443b3a9e30d0939d0
chrisnycvillage-cyber <chrisnycvillage@gmail.com>
2025-12-08 08:35:43 -0500

    Update crons.ts

--- convex/crons.ts
+++ convex/crons.ts
@@ -1,13 +1,15 @@
 import { cronJobs } from "convex/server";
-import { internal } from "./_generated/api";
+// import { internal } from "./_generated/api";
 
 const crons = cronJobs();
 
-// Run Instagram scrape every 6 hours
-crons.interval(
-  "scrape-instagram-stats",
-  { hours: 6 },
-  internal.apify.runScheduledScrape
-);
+// DISABLED - Auto-scraping turned off
+// Data is now manually managed to match Metrix Media
+// 
+// crons.interval(
+//   "scrape-instagram-stats",
+//   { hours: 6 },
+//   internal.apify.runScheduledScrape
+// );
 
 export default crons;
